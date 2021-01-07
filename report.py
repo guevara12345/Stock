8c8642b57f5507220dd9240291a8958a33948ce7
wangyu <wangyu_xjtu@163.com>
2021-01-07 18:36:02 +0800

    1

--- report.py
+++ report.py
@@ -6,8 +6,7 @@ import time
 
 
 from strategy_new_highest import new_high
-from code_formmat import code_formatter
-
+from strategy_double_ma import double_ma
 
 
 def generate_hs300_report():
@@ -18,6 +17,12 @@ def generate_hs300_report():
     for code in hs300_df.index.values.tolist():
         hs300_df.loc[code, 'highest_date'] = new_high.new_highest_date_with_xiuquedata(
             code)
+
+        a, b = double_ma.double_ma_13_21(code)
+        hs300_df.loc[code, '(price,ma13,ma21)'] = '{};{:.4};{:.4}'.format(
+            a[0], a[1], a[2])
+        hs300_df.loc[code, '(price-ma21,price-ma13,diff)'] = '{:.2%};{:.2%};{:.2%}'.format(
+            b[0], b[1], b[2])
 
     filename = 'hs300_report_{}_{}'.format(
         datetime.now().strftime('%Y%B%d'), int(time.time()))
@@ -35,12 +40,19 @@ def generate_zz500_report():
         zz500_df.loc[code, 'highest_date'] = new_high.new_highest_date_with_xiuquedata(
             code)
 
+        a, b = double_ma.double_ma_13_21(code)
+        zz500_df.loc[code, '(price,ma13,ma21)'] = '{};{:.4};{:.4}'.format(
+            a[0], a[1], a[2])
+        zz500_df.loc[code, '(price-ma21,price-ma13,diff)'] = '{:.2%};{:.2%};{:.2%}'.format(
+            b[0], b[1], b[2])
+
     filename = 'zz500_report_{}_{}'.format(
         datetime.now().strftime('%Y%B%d'), int(time.time()))
     zz500_df.to_csv(
         os.path.join(os.getcwd(), f'raw_data/{filename}.csv'),
         encoding="gbk")
 
+
 if __name__ == '__main__':
     generate_hs300_report()
-    generate_zz500_report()
+    # generate_zz500_report()
--- strategy_double_ma.py
+++ strategy_double_ma.py
@@ -4,7 +4,29 @@ from downloader import bao_d, xueqiu_d
 from code_formmat import code_formatter
 
 
-def double_ma(code):
-    capital_code = code_formatter.code2capita(code)
-    dataFrame = xueqiu_d.download_dkline_from_xueqiu(capital_code, 10*5)
-    
+class StrategyDoubleMa:
+    def double_ma_13_21(self, code):
+        capital_code = code_formatter.code2capita(code)
+        dataFrame = xueqiu_d.download_dkline_from_xueqiu(capital_code, 10*5)
+        dataFrame['MA13'] = dataFrame['close'].ewm(
+            span=13, adjust=False).mean()
+        dataFrame['MA21'] = dataFrame['close'].ewm(
+            span=21, adjust=False).mean()
+        dataFrame['PRICE-MA21'] = dataFrame['close']-dataFrame['MA21']
+        dataFrame['PRICE-MA13'] = dataFrame['close']-dataFrame['MA13']
+        dataFrame['DIFF'] = dataFrame['MA13']-dataFrame['MA21']
+        dataFrame = dataFrame.sort_values(by='date', ascending=False)
+        a = (
+            dataFrame.iloc[0]['close'],
+            dataFrame.iloc[0]['MA13'],
+            dataFrame.iloc[0]['MA21']
+        )
+        b = (
+            dataFrame.iloc[0]['PRICE-MA21']/dataFrame.iloc[0]['close'],
+            dataFrame.iloc[0]['PRICE-MA13']/dataFrame.iloc[0]['close'],
+            dataFrame.iloc[0]['DIFF']/dataFrame.iloc[0]['close']
+        )
+        return a, b
+
+
+double_ma = StrategyDoubleMa()
